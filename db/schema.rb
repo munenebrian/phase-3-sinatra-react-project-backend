817ecdb12a4296a0724fe5b0ac290326f1c0d397
munenebrian <njerubrianmunene@gmail.com>
2022-09-10 17:29:13 +0300

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_135722) do
+ActiveRecord::Schema.define(version: 2022_09_10_140022) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "title"
     t.integer "authors_id"
     t.integer "categories_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "categories", force: :cascade do |t|
