3f5a670a2fb878c11cc76ba1664eb4280293c161
munenebrian <njerubrianmunene@gmail.com>
2022-09-10 11:21:21 +0300

    changes

---
+++ app/controllers/assignment_controller.rb
@@ -0,0 +1,33 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+# ALL POEMS
+get '/works' do
+  works = Work.all
+ works.to_json
+
+end
+
+post "/works" do
+  works = Work.create(
+    title: params[:title],
+    body: params[:body],
+    authors_id: params[:authors_id],
+    categories_id: params[:categories_id])
+  works.to_json
+
+end
+
+delete "/works/:id" do
+  works = Work.find(params[:id])
+  works.destroy
+  works.to_json
+end
+
+patch "/workcategories/:id"  do
+  categories = Category.find(params[:id])
+  categories.update(
+    title: params[:title],
+    body: params[:body]
+  )
+end
+end
